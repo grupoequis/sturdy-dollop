f3ad1c3b7d17f7656f33a6a71fb4d475862240d4
yammahi <ahmed34@gl.umbc.edu>
2019-09-24 18:25:12 -0500

    added get and signal handler

--- tftp_prueba.py
+++ tftp_prueba.py
@@ -1,4 +1,4 @@
-    	#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
 
@@ -6,11 +6,14 @@
 Usage:
   tftp-client.py get <filename> [[-s | -b ] --mode=<mode>]
 """
+import signal
 import argparse
+import sys
 from socket import *
 from struct import pack
 from struct import unpack
 mode_list = {'unknown': 0,'netascii': 1,'octet': 2,'mail': 3}
+
 
 error_message = {
     0: "Not defined, see error message (if any).",
@@ -28,27 +31,7 @@ server_address = ('localhost', 69)
 ackn = 0
 port = 69
 sent_address = ('localhost',69)
-
-
-def binary_rrq(filename, mode):
-	request = bytearray()
-	# READ REQUEST
-	request.append(0)	
-	request.append(1)	
-	filename = bytearray(filename.encode('utf-8'))
-	request += filename	
-	request.append(0)
-	request += bytearray(bytes(mode, 'utf-8'))
-	request.append(0)
-	sock.sendto(request, server_address)
-
-
-def struct_rrq(filename, mode):
-    formatter = '>h{}sB{}sB'  # { > - Big Endian, h - short , s - char, B - 1 byte }
-    formatter = formatter.format(len(filename), len('netascii'))
-    request = pack(formatter, 1, bytes(filename, 'utf-8'), 0, bytes(mode, 'utf-8'), 0)
-    sock.sendto(request, server_address)
-
+mode = "netascii"
 
 def sendACK(ack_data, server):
 	ack = bytearray(ack_data)
@@ -57,19 +40,54 @@ def sendACK(ack_data, server):
 	sock.sendto(ack, server)
 
 def console():
-	print("tftp>",end='')
-	cmd = input()
-	cmds = cmd.split(" ")
-	if(cmds[0] == "put"):
-		put(cmds[1])
+	try:
+		while True:
+			print("tftp>",end='')
+			cmd = input()
+			cmds = cmd.split(" ")
+			if(cmds[0] == "put"):
+				put(cmds[1])
+			elif(cmds[0] == 'get'):
+				get(cmds[1])
+			elif(cmds[0] == 'quit'):
+				exit()
+	except EOFError as error:
+		exit()
+
+
+def get(filename):
+	request = bytearray()
+	# READ REQUEST
+	request.append(0)	
+	request.append(1)	  
+	request += bytearray(filename.encode('utf-8'))
+	request.append(0)
+	request += bytearray(bytes(mode, 'utf-8'))
+	request.append(0)
+	sock.sendto(request, server_address)
+
+	file = open(filename, "wb")
+	while True:
+		# Wait for the data from the server
+		data, server = sock.recvfrom(600)
+		if error(data):
+			error_code = int.from_bytes(data[2:4], byteorder='big')
+			print(error_message[error_code])
+			break
+		sendACK(data[0:4], server)
+		file.write(data[4:])
+		# Last block
+		if len(data) < 516:
+		    	break
+
+
 def put(filename):
-	print('sending file')
 	request = bytearray()
 	request.append(0)
 	request.append(2)
 	request += bytearray(filename.encode('utf-8'))
 	request.append(0)
-	request += bytearray("netascii".encode('utf-8'))
+	request += bytearray(mode.encode('utf-8'))
 	request.append(0)
 	sock.sendto(request,server_address)
 	res = waitack()
@@ -98,7 +116,6 @@ def waitack():
 		sent_address = address
 		ack = 4
 		results = unpack('>'+'h'*(len(data)//2),data)
-		print("ack: %d %d" % (results[0],results[1]))
 		if ack == results[0] and ackn == results[1]:
 			ackn += 1
 			return "ack"
@@ -109,54 +126,19 @@ def error(data):
 	opcode = data[:2]
 	return int.from_bytes(opcode, byteorder='big') == 5
 
+def signal_handler(sig, frame):
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 def main():
+	if(len(sys.argv) > 1):
+		server_address = (sys.argv[1],69)
 	console()
-	print("terminando")
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-b", "--binary", help="request built with bytearray", action="store_true")
-	parser.add_argument("-s", "--struct", help="request built with struct", action="store_true")
-	parser.add_argument("-f", "--file", help="file")
-	parser.add_argument("-mode", "--mode", help="TFTP transfer mode")
-	args = parser.parse_args()
-	parser.print_help()
- 
-	# check arguments
-	if args.binary:
-	    print ("En bytearray")
-	if args.struct:
-	    print ("En struct")
-	if args.file:
-	    print ("Filename: ", args.file)
-	if args.mode:
-	    print ("Mode: ", args.mode)
-
-	mode = args.mode
-	if mode is not None:
-		if mode.lower() not in mode_list.keys():
-			print ("Unknown mode. Mode: netascci")		    
-			mode = "netascii"
-	else:
-		mode = "netascii"
-	
-	if args.binary:
-		binary_rrq(args.file, mode)
-	elif args.struct:
-		struct_rrq(args.file, mode)
-	else: 	
-		struct_rrq(args.file, mode)
-
-	file = open(args.file, "wb")
-	while True:
-		# Wait for the data from the server
-		data, server = sock.recvfrom(600)
-		if error(data):
-			error_code = int.from_bytes(data[2:4], byteorder='big')
-			print(error_message[error_code])
-			break
-		sendACK(data[0:4], server)
-		file.write(data[4:])
-		# Last block
-		if len(data) < 516:
-		    	break
+	#parser = argparse.ArgumentParser()
+	#parser.add_argument("-b", "--binary", help="request built with bytearray", action="store_true")
+	#parser.add_argument("-s", "--struct", help="request built with struct", action="store_true")
+	#parser.add_argument("-f", "--file", help="file")
+	#parser.add_argument("-mode", "--mode", help="TFTP transfer mode")
 if __name__ == '__main__':
     main()
